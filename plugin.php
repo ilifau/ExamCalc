491793ec3754fde2118ae44aef22155a1c5d20c6
André Schweigert <andre.schweigert@ili.fau.de>
2025-07-15 11:53:42 +0200

    Backport ILIAS 7

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $plugin_name         = "ExamCalc";
 $id                  = "examcalc";
-$version             = "1.0.4";
+$version             = "1.0.5";
 $ilias_min_version   = "7.0";
 $ilias_max_version   = "7.999";
 $responsible         = "André Schweigert";
